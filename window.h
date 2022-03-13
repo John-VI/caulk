903a72811cd19305efea20a6fc54a4386f812d62
John Whitley <dogjaw2233@gmail.com>
2022-03-12 22:57:56 -0500

    Erased redundant files

--- window.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) John Allen Whitley, 2022, BSD 3-Clause
-
-#ifndef CAULK_WINDOW_H
-#define CAULK_WINDOW_H
-
-SDL_Window *CLK_NewWindow(const char name[], int width, int height);
-SDL_Renderer *CLK_NewRenderer(SDL_Window *win, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
-
-#endif /* CAULK_WINDOW_H */
